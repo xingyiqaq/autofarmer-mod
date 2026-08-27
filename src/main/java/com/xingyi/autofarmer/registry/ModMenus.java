378319bdf287edca03247cb6581fe9ea66528627
xingyiqaq <xingyi@users.noreply.github.com>
2026-08-28 02:03:18 +0800

    fix: use IForgeMenuType.create and MenuType for 1.20.1

--- src/main/java/com/xingyi/autofarmer/registry/ModMenus.java
+++ src/main/java/com/xingyi/autofarmer/registry/ModMenus.java
@@ -1,7 +1,6 @@
 package com.xingyi.autofarmer.registry;
 
 import com.xingyi.autofarmer.AutoFarmerMod;
-import com.xingyi.autofarmer.menu.AutoFarmerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,7 +11,8 @@ public class ModMenus {
     public static final DeferredRegister<MenuType<?>> MENUS =
         DeferredRegister.create(ForgeRegistries.MENU_TYPES, AutoFarmerMod.MOD_ID);
 
-    public static final RegistryObject<MenuType<AutoFarmerMenu>> AUTOFARMER =
-        MENUS.register("autofarmer_menu",
-            () -> IForgeMenuType.create((id, inv, data) -> new AutoFarmerMenu(id, inv, data)));
+    public static final RegistryObject<MenuType<?>> AUTOFARMER =
+        MENUS.register("autofarmer", () -> IForgeMenuType.create((windowId, inv, data) -> {
+            return new com.xingyi.autofarmer.menu.AutoFarmerMenu(windowId, inv, data);
+        }));
 }
