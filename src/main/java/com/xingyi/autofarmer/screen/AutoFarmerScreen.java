8a226b1ea443aa5ea3c2e679477ecc075a18edb3
xingyiqaq <xingyi@users.noreply.github.com>
2026-08-28 01:17:24 +0800

    fix: update screen for MC 1.20.1 GuiGraphics API

--- src/main/java/com/xingyi/autofarmer/screen/AutoFarmerScreen.java
+++ src/main/java/com/xingyi/autofarmer/screen/AutoFarmerScreen.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.xingyi.autofarmer.AutoFarmerMod;
 import com.xingyi.autofarmer.menu.AutoFarmerMenu;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
@@ -13,7 +14,7 @@ import net.minecraft.world.entity.player.Inventory;
 public class AutoFarmerScreen extends AbstractContainerScreen<AutoFarmerMenu> {
 
     private static final ResourceLocation TEXTURE =
-        new ResourceLocation(AutoFarmerMod.MOD_ID, "textures/gui/autofarmer.png");
+        ResourceLocation.fromNamespaceAndPath(AutoFarmerMod.MOD_ID, "textures/gui/autofarmer.png");
 
     public AutoFarmerScreen(AutoFarmerMenu menu, Inventory inv, Component title) {
         super(menu, inv, title);
@@ -26,30 +27,27 @@ public class AutoFarmerScreen extends AbstractContainerScreen<AutoFarmerMenu> {
     }
 
     @Override
-    protected void renderBg(PoseStack poseStack, float partialTick, int mouseX, int mouseY) {
+    protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
         RenderSystem.setShaderTexture(0, TEXTURE);
         int x = (this.width - this.imageWidth) / 2;
         int y = (this.height - this.imageHeight) / 2;
-        this.blit(poseStack, x, y, 0, 0, this.imageWidth, this.imageHeight);
+        graphics.blit(TEXTURE, x, y, 0, 0, this.imageWidth, this.imageHeight);
     }
 
     @Override
-    public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
-        this.renderBackground(poseStack);
-        super.render(poseStack, mouseX, mouseY, partialTick);
-        this.renderTooltip(poseStack, mouseX, mouseY);
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+        this.renderBackground(graphics);
+        super.render(graphics, mouseX, mouseY, partialTick);
+        this.renderTooltip(graphics, mouseX, mouseY);
     }
 
     @Override
-    protected void renderLabels(PoseStack poseStack, int mouseX, int mouseY) {
-        // Draw slot labels
+    protected void renderLabels(GuiGraphics graphics, int mouseX, int mouseY) {
         int x = (this.width - this.imageWidth) / 2;
         int y = (this.height - this.imageHeight) / 2;
-        // Input label (above input slots)
-        this.font.draw(poseStack, "Input", x + 8, y + 26, 0xC8C8C8);
-        // Output label (above output slot)
-        this.font.draw(poseStack, "Output", x + 104, y + 26, 0xC8C8C8);
+        graphics.drawString(this.font, "Input", x + 8, y + 26, 0xC8C8C8, false);
+        graphics.drawString(this.font, "Output", x + 104, y + 26, 0xC8C8C8, false);
     }
 }
