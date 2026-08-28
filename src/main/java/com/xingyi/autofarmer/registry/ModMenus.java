ea56fa2a5d0caf6f0965b25eca7e9e45f3ef2b13
xingyiqaq <xingyi@users.noreply.github.com>
2026-08-29 01:02:11 +0800

    fix: typed MenuType<AutoFarmerMenu>

--- src/main/java/com/xingyi/autofarmer/registry/ModMenus.java
+++ src/main/java/com/xingyi/autofarmer/registry/ModMenus.java
@@ -1,6 +1,7 @@
 package com.xingyi.autofarmer.registry;
 
 import com.xingyi.autofarmer.AutoFarmerMod;
+import com.xingyi.autofarmer.menu.AutoFarmerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -8,11 +9,11 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModMenus {
+    @SuppressWarnings("unchecked")
     public static final DeferredRegister<MenuType<?>> MENUS =
         DeferredRegister.create(ForgeRegistries.MENU_TYPES, AutoFarmerMod.MOD_ID);
 
-    public static final RegistryObject<MenuType<?>> AUTOFARMER =
-        MENUS.register("autofarmer", () -> IForgeMenuType.create((windowId, inv, extraData) -> {
-            return new com.xingyi.autofarmer.menu.AutoFarmerMenu(windowId, inv, extraData);
-        }));
+    public static final RegistryObject<MenuType<AutoFarmerMenu>> AUTOFARMER =
+        MENUS.register("autofarmer", () -> IForgeMenuType.create((windowId, inv, extraData) ->
+            new AutoFarmerMenu(windowId, inv, extraData)));
 }
